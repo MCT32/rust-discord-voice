abdf79a5b300428550d5a3f84535b71cbd346d92
MCT32 <ivanhen99@gmail.com>
2024-10-02 19:23:43 +1000

    Logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use serde_json::Value;
+use log::debug;
 
 fn main() {
+    // Set up logger
+    simple_logger::init_with_level(
+        if cfg!(debug_assertions) {
+            log::Level::Trace
+        } else {
+            log::Level::Info
+        }
+    ).unwrap();
+
+    // TODO: Cache gateway url like specified in documentation
     let resp = serde_json::from_str::<Value>(
         reqwest::blocking::get("https://discord.com/api/v10/gateway").unwrap()
             .text().unwrap()
@@ -10,5 +21,5 @@ fn main() {
     let gateway_url = resp.get("url").unwrap()
         .as_str().unwrap();
 
-    println!("{}", gateway_url);
+    debug!("Gateway url is {}", gateway_url)
 }
