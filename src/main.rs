715ab7e8d48a1510970412685e48bfd1b67d6d2c
MCT32 <ivanhen99@gmail.com>
2024-10-02 19:41:05 +1000

    Replaced unwraps with expects

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,22 @@ fn main() {
         } else {
             log::Level::Info
         }
-    ).unwrap();
+    ).expect("Failed to initialise logger");
 
     // TODO: Cache gateway url like specified in documentation
     let resp = serde_json::from_str::<Value>(
-        reqwest::blocking::get("https://discord.com/api/v10/gateway").unwrap()
-            .text().unwrap()
+        reqwest::blocking::get("https://discord.com/api/v10/gateway")
+            .expect("Failed to get discord gateway url")
+            // TODO: Add a test to make sure responce was ok (2xx)
+            .text().expect("Responce from discord gateway endpoint had no body")
             .as_str()
-    ).unwrap();
+    ).expect("Discord gateway endpoint responce could not be parsed as json");
 
-    let gateway_url = resp.get("url").unwrap()
-        .as_str().unwrap();
+    let gateway_url = resp.get("url").expect("Discord gateway endpoint reponse had to url key")
+        .as_str().expect("Discord gateway endpoint responce had an unexpected type");
 
     debug!("Gateway url is {}", gateway_url)
+
+
+    
 }
